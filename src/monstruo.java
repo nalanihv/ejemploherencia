d7784570366e109ddfb1067c1d4fd33c78d1cd21
Nalanihv <nalanihevi@gmail.com>
2022-03-23 11:47:13 -0600

    Initial commit

---
+++ src/monstruo.java
@@ -0,0 +1,19 @@
+//extends nombre_clase_padre
+
+public class monstruo extends personaje {
+    //atributos:
+    /* cuernos
+    tentaculos
+    color
+     */
+    
+    public monstruo(String nombre, double estatura, double peso) {
+        super(nombre, estatura, peso);
+    }
+
+    //metodos
+    /*espantar
+    trabajar
+    hacer_reir
+     */
+}
---
+++ src/personaje.java
@@ -0,0 +1,47 @@
+public class personaje {
+    //nombre
+    //estatura
+    //peso
+    protected  String nombre;
+    protected double estatura;
+    protected double peso;
+
+    //comer
+    //dormir
+    //pensar
+
+    public personaje( String nombre, double estatura, double peso) {
+        this.nombre = nombre;
+        this.estatura = estatura;
+        this.peso = peso;
+        System.out.println("estoy en el constructor padre");
+    }
+
+    public String getNombre() {
+        return nombre;}
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;}
+
+    public double getEstatura() {
+        return estatura;}
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;}
+
+    public double getPeso() {
+        return peso;}
+
+    public void setPeso(double peso) {
+        this.peso = peso;}
+
+    public void comer(){
+        System.out.println("coiendo");
+    }
+    public void  dormir (){
+        System.out.println("durmiendo");
+    }
+    public void pensar (){
+        System.out.println("pensando");
+    }
+}
